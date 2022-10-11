126fd71da547c30fcee242e087a1f6a4c6f72164
Larissa Kich <76120366+Larissakich@users.noreply.github.com>
2022-10-11 14:41:00 -0300

    Add files via upload

---
+++ Gerador de numeros aleatorios/assets/js/script.js
@@ -0,0 +1,14 @@
+const button = document.getElementById('generate');
+
+button.addEventListener('click', function() {
+    const min = parseInt(document.getElementById('min').value);
+    const max = parseInt(document.getElementById('max').value);
+
+    let result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    if (isNaN(result)) {
+        result = 'Valor inválido';
+    }
+
+    document.querySelector('#result > span').textContent = result;
+});
